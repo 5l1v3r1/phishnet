add9ffa59800c5f9ccd27bed323d4aa0b76ec0cc
Andrew Huang Work <ahuang@liquidweb.com>
2018-01-11 16:36:23 -0500

    add cymruwhois package

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # phishnet
 to catch phish
+
+liquidweb ASN
+AS32244
+AS53824
+AS201682
---
+++ iptest.py
@@ -0,0 +1,8 @@
+import socket
+from cymruwhois import Client
+
+ip = socket.gethostbyname('nospaceindent.com')
+
+c = Client()
+r = c.lookup(ip)
+print(r.asn)
