577a6269c5dbf9913ef0ec14a8ae0a95c1c2a752
Andrew Huang Work <ahuang@liquidweb.com>
2018-01-16 14:17:22 -0500

    adds suspicious.py

--- phishnet.py
+++ phishnet.py
@@ -40,9 +40,11 @@ def print_callback(message, context):
             domain = all_domains[0]
             success, ip = in_network(domain)
             if success:
-                sys.stdout.write(u"{} - {} {} (SAN: {})\n".format(datetime.datetime.now().strftime('%m/%d/%y %H:%M:%s'), ip, domain, ", ".join(message['data']['leaf_cert']['all_domains'][1:])))
+                sys.stdout.write(u"{} - {} {} (SAN: {})\n".format(datetime.datetime.now().strftime('%m/%d/%y %H:%M'), ip, domain, ", ".join(message['data']['leaf_cert']['all_domains'][1:])))
                 sys.stdout.flush()
             
+        sys.stdout.write(u"[{}] {} (SAN: {})\n".format(datetime.datetime.now().strftime('%m/%d/%y %H:%M:%S'), domain, ", ".join(message['data']['leaf_cert']['all_domains'][1:])))
+        sys.stdout.flush()
 
 #logging.basicConfig(format='[%(levelname)s:%(name)s] %(asctime)s - %(message)s', level=logging.INFO)
 
